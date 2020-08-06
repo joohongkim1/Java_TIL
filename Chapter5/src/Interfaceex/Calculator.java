a13380be2334c4d73d2fa3b840d88b7a53fbc16d
joohongkim1 <wnghd9938@naver.com>
2020-08-06 17:33:09 +0900

    start Interface

---
+++ Chapter5/src/Interfaceex/Calculator.java
@@ -0,0 +1,28 @@
+package Interfaceex;
+
+public abstract class Calculator implements Calc {
+
+	@Override
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	// 일부만 구현하게 될 경우 해당 클래스는 추상 클래스가 된다.
+//	@Override
+//	public int times(int num1, int num2) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	@Override
+//	public int divide(int num1, int num2) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+
+}
